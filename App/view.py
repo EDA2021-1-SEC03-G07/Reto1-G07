aa97b8f1c1d5de83d902b86499cfd3e20063c21a
NicoleMurillo <n.murillof@uniandes.edu.co>
2021-02-22 21:38:59 -0500

    Avance Req 1 ordenamientos iterativos – Reto 1

--- App/view.py
+++ App/view.py
@@ -53,7 +53,7 @@ def loadData(catalog):
 
     controller.loadData(catalog)
 
-def printResults(sortedVideos, sample):
+def printResults(sortedVideos, sample=10):
     size = lt.size(sortedVideos)
     if size > sample:
         print("Los primeros ", sample, " videos ordenados son: ")
@@ -63,8 +63,6 @@ def printResults(sortedVideos, sample):
             print('Vistas: ' + video['views'] + ' Título: ' + video['title'] + ' Fecha de tendencia: ' + video['trending_date'] + ' Nombre del canal: ' + 
             video['channel_title'] + ' Hora de publicación: ' + video['publish_time'] + ' Likes: ' + video['likes'] + ' Dislikes ' + video['dislikes'])
             i +=1
-    if size < sample:
-        print("El número indicado excede el tamaño de la muestra cargada.")
 
 """
 Menu principal
@@ -91,10 +89,8 @@ while True:
         else:
             iterative = input("Indique el tipo de algoritmo de ordenamiento iterativo: ")
             result = controller.sortVideosByViews(catalog, int(size), iterative)
-            sample = int(input("Indique el número n de elementos que desea en la lista: "))
             print("Para la muestra de " , size, " elementos, el tiempo (mseg) es: ", str(result[0]))
-            printResults(result[1], sample)
-    
+            printResults(result[1])
     else:
         sys.exit(0)
 sys.exit(0)
